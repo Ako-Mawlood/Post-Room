42dc783186ea9aa9e4f9da4c43c0b977c23fb4a8
Ako-Mawlood <ako.mawlood01@gmail.com>
2024-06-06 17:29:58 +0300

    Making landing page responsive & fixing footer issue.

--- app/Components/pages/Landing/BlogsPreview.tsx
+++ app/Components/pages/Landing/BlogsPreview.tsx
@@ -1,117 +1,55 @@
+import { previewBlogs } from "@/StaticData/previewBlogs";
 import Image from "next/image";
-import { Avatar, AvatarImage, AvatarFallback } from "../../ui/avatar"
-import { Button } from "../../ui/button";
+import clsx from "clsx";
 import starIcon from "../../../../public/Icons/star.svg"
-
-interface blogType {
-    id: number;
-    fullName: string;
-    authorBio: string;
-    authorImage: string;
-    title: string;
-    description: string;
-    src: string;
-}
-
-const previewBlogs: blogType[] = [
-    {
-        id: 0,
-        title: "Impacts of Adult Behaviors on Children",
-        description: "Explore how adult actions and behaviors influence children's development.",
-        src: "https://tse2.mm.bing.net/th/id/OIG1.69oXxCxgb5GiBZw6Rz4m?pid=ImgGn",
-        fullName: "Sangar Mawlood",
-        authorImage: "https://scontent.febl5-2.fna.fbcdn.net/v/t39.30808-6/274879156_2177696605718753_4655404325092292858_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=LDZ-1TfT64AQ7kNvgEPvJNF&_nc_ht=scontent.febl5-2.fna&oh=00_AYCJP1XZLdZ3WLgvJJ4sm0igh_QZOpCnJE2osYXn1CL2FA&oe=66666DAA",
-        authorBio: "Psychologist, Author"
-    },
-    {
-        id: 1,
-        title: "The Importance of Sleep for Children",
-        description: "Explore how adequate sleep positively impacts children's health and development.",
-        src: "https://tse2.mm.bing.net/th/id/OIG3.mdSgx14Ax0H725buTlml?pid=ImgGn",
-        fullName: "Jordan Smith",
-        authorImage: "https://as1.ftcdn.net/v2/jpg/03/02/88/46/1000_F_302884605_actpipOdPOQHDTnFtp4zg4RtlWzhOASp.jpg",
-        authorBio: "Doctor, Writer"
-    },
-    {
-        id: 2,
-        title: "The Rapid Economic Growth of Kurdistan",
-        description: "An in-depth look at the factors driving Kurdistan's economic development.",
-        src: "https://images.pexels.com/photos/1209978/pexels-photo-1209978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        fullName: "Mohammed",
-        authorImage: "https://images.peFxels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        authorBio: "Engineer, Writer"
-    },
-    {
-        id: 3,
-        title: "The Importance of Calmness",
-        description: "Explore how staying calm can improve your overall well-being.",
-        src: "https://images.pexels.com/photos/9002742/pexels-photo-9002742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        fullName: "Peirs Mosh",
-        authorImage: "https://www.wilsoncenter.org/sites/default/files/styles/large/public/media/images/person/james-person-1.webp",
-        authorBio: "Innovator"
-    },
-    {
-        F
-        id: 4,
-        title: "The Effects of Loneliness",
-        description: "Understand the impact of loneliness on mental health and ways to combat it.",
-        src: "https://tse1.mm.bing.net/th/id/OIG1.KwznMBY4NglXceoosm2u?pid=ImgGn",
-        fullName: "Karim Abadulla",
-        authorImage: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        authorBio: "Psychologist, Author"
-    },
-    {
-        id: 5,
-        title: "The Joy of Coding",
-        description: "Dive into the world of coding and discover how it can unlock endless possibilities.",
-        src: "https://tse4.mm.bing.net/th/id/OIG3.AkxPxsozuQr7CoBPgUoe?pid=ImgGn",
-        fullName: "Abdlbarry Qaisar",
-        authorImage: "https://as1.ftcdn.net/v2/jpg/02/43/12/34/1000_F_243123463_zTooub557xEWABDLk0jJklDyLSGl2jrr.jpg",
-        authorBio: "Software Developer, Educator"
-    }
-];
-
+import { Avatar, AvatarImage, AvatarFallback } from "../../ui/avatar"
+import { Badge } from "../../ui/badge";
 
 interface BlogsPreviewPropsType {
     sliderIndex: number;
     backgroundColor: string
 }
-const BlogsPreview = ({ sliderIndex, backgroundColor }: BlogsPreviewPropsType) => {
 
+const BlogsPreview = ({ sliderIndex, backgroundColor }: BlogsPreviewPropsType) => {
     return (
-        <div style={{ backgroundColor: `rgb(${backgroundColor})` }} className="w-full md:w-[720px] relative border-y-[1px] border-black md:border-none">
+        <div style={{ backgroundColor: `rgb(${backgroundColor})` }} className="w-full  md:w-[720px] h-[70vh] md:h-auto relative border-y-[1px] border-black md:border-none">
             {
-                previewBlogs.map((blog: blogType, index: number) => (
-                    <div key={blog.id} className={clsx("h-[70vh] md:h-full w-full absolute top-0 left-0", {
-                        "opacity-0 duration-500": sliderIndex != index,
-                        "opacity-100 duration-500": sliderIndex == index,
+                previewBlogs.map((blog, index) => (
+                    <div key={blog.id} className={clsx("h-[70vh] md:h-full w-full absolute top-0 left-0 duration-500", {
+                        "opacity-0": sliderIndex != index,
+                        "opacity-100": sliderIndex == index,
                     })}>
                         <div className="flex flex-col md:flex-row w-full h-3/5 absolute md:relative">
-                            <div style={{ background: `linear-gradient(0deg, rgb(${backgroundColor}) 0, rgba(0,0,0,0) 50%)` }} className="w-full h-full  relative bottom-[-2px] left-0 z-40">
-                                <Button variant="secondary" className="flex items-center gap-3 text-xs absolute top-[-20px] rounded-none opacity-95 shadow-sm mt-8 z-50 right-4">
+                            <div
+                                style={{ background: `linear-gradient(0deg, rgb(${backgroundColor}) 0, rgba(0,0,0,0) 50%)` }}
+                                className="w-full h-full relative bottom-[-2px] left-0 z-40">
+                                <Badge
+                                    variant="secondary"
+                                    className="flex items-center gap-3 absolute md:bottom-[-15px] left-4 mt-8 rounded-none md:rounded-full text-xs opacity-85  z-50">
                                     <Image
                                         className="size-6"
                                         src={starIcon}
                                         alt="Star icon"
                                     />
                                     Get this & many more
-                                </Button>
+                                </Badge>
                             </div>
                             <Image
                                 src={blog.src}
-                                className="object-cover w-full h-full z-30"
+                                className="w-full h-full object-cover z-30"
                                 fill={true}
                                 alt="Blog image"
                             />
                         </div>
-                        <div className="px-4 pt-10 md:px-5 md:py-7 lg:py-10">
-                            <div style={{ backgroundColor: `rgb(${backgroundColor})` }} className="flex flex-col self-center gap-3 w-full h-2/5 absolute bottom-0 left-0 md:relative p-4 sm:pr-10 md:p-0 border-black font-semibold z-50">
-                                <h1 className="text-2xl line-clamp-2 opacity-75">{blog.title}</h1>
-                                <p className="opacity-70 text-base md:line-clamp-2 line-clamp-4">{blog.description}</p>
-                                <div className="flex items-center gap-3 font-Satoshi">
+                        <div className="md:px-5 md:py-7 lg:py-10">
+                            <div style={{ backgroundColor: `rgb(${backgroundColor})` }} className="flex flex-col gap-2 w-full md:h-2/5 absolute md:relative bottom-0 left-0 p-4 mb-8 md:m-0 md:p-0 font-semibold z-50">
+                                <h1 className="text-2xl md:line-clamp-2 opacity-75">{blog.title}</h1>
+                                <p className="text-base opacity-70 md:line-clamp-2">{blog.description}</p>
+                                <div className="flex items-center gap-3">
                                     <Avatar>
                                         <AvatarImage src={blog.authorImage} />
-                                        <AvatarFallback>AM</AvatarFallback>
+                                        <AvatarFallback> {blog.fullName.split(" ").slice(0, 2).map(word => word[0].toUpperCase())}
+                                        </AvatarFallback>
                                     </Avatar>
                                     <div className="text-base font-sans">
                                         <h2 className="opacity-75">{blog.fullName}</h2>
@@ -119,7 +57,6 @@ const BlogsPreview = ({ sliderIndex, backgroundColor }: BlogsPreviewPropsType) =
                                     </div>
                                 </div>
                             </div>
-
                         </div>
                     </div>
                 ))
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,27 @@
 "use client"
 
+import { backgroundColors } from "@/StaticData/backgroundColors"
 import logo from "../public/Icons/logo.svg"
 import Image from "next/image"
 import { Button } from "./Components/ui/button"
 import BlogsPreview from "./Components/pages/Landing/BlogsPreview"
-import useSlider from "./Hooks/useSlider.ts"
+import useSlider from "./Hooks/useSlider"
+import Link from "next/link"
 
 export default function LandingPage() {
   const { sliderIndex } = useSlider(6)
-  const backgroundColors = [
-    "247, 247, 247",
-    "238,148,229",
-    "178,214,141",
-    "238,148,229",
-    "253, 210, 119",
-    "182,201,217"
-  ];
   const backgroundColor = backgroundColors[sliderIndex]
 
   return (
     <>
-      <nav className="flex justify-between items-center w-screen h-[8vh] px-4 md:px-24 bg-gray-100">
-        <div className="flex items-center font-bold text-xl sm:text-2xl">
+      <nav className="flex justify-between items-center w-screen h-[8vh] px-4 md:px-24 py-1 bg-gray-100">
+        <Link href="/" className="flex items-center font-bold text-lg sm:text-2xl">
           <Image
             src={logo}
             alt="Post-Room logo"
           />
           <h1>Post-Room</h1>
-        </div>
+        </Link>
         <div className="flex text-gray-800">
           <Button variant="ghost" className="hidden sm:block">Explore</Button>
           <Button variant="ghost" className="hidden sm:block">Write</Button>
@@ -35,24 +29,22 @@ export default function LandingPage() {
           <Button>Get Started</Button>
         </div>
       </nav>
-      <main className="w-full relative font-Pt">
-        <section className="flex flex-col md:flex-row justify-between md:h-[84vh] w-screen border-y-[1px] border-black duration-700">
-          <div style={{ backgroundColor: `rgb(${backgroundColor},0.3)` }} className="flex flex-col gap-6 py-10 sm:py-20 px-4 md:px-24 md:border-r-[1px] md:border-black">
-            <h1 className="text-5xl md:text-[7vw] leading-tight">Your story matters</h1>
-            <p className='text-xl opacity-80'>Share your unique stories with a global audience. Join us today and make an impact with your words.</p>
-            <Button className="w-36">Get started</Button>
-          </div>
-          <BlogsPreview sliderIndex={sliderIndex} backgroundColor={backgroundColor} />
-        </section>
-        <footer className="hidden md:flex justify-center items-center h-[8vh] w-screen bottom-0 left-0 text-gray-700 bg-gray-100">
-          <Button variant="ghost">Help</Button>
-          <Button variant="ghost">Explore</Button>
-          <Button variant="ghost">Terms</Button>
-          <Button variant="ghost">Polices</Button>
-          <Button variant="ghost">Signin</Button>
-          <Button variant="ghost">Signup</Button>
-        </footer>
+      <main className="flex flex-col md:flex-row justify-start w-full md:h-[84vh] relative border-y-[1px] border-black duration-700 font-Pt selection:text-gray-200 selection:bg-gray-800">
+        <div style={{ backgroundColor: `rgb(${backgroundColor},0.3)` }} className="flex flex-col gap-6 justify-center items-start p-4 py-10 md:px-24 md:border-r-[1px] md:border-black">
+          <h1 className="text-5xl md:text-[7vw] leading-tight">Your story matters</h1>
+          <p className='text-xl opacity-80'>Share your unique stories with a global audience. Join us today and make an impact with your words.</p>
+          <Button className="w-36">Get started</Button>
+        </div>
+        <BlogsPreview sliderIndex={sliderIndex} backgroundColor={backgroundColor} />
       </main>
+      <footer className="flex justify-center items-center h-[8vh] w-screen px-4 border-y-[1px] border-black md:border-none bg-gray-100 overflow-hidden">
+        <Button variant="ghost">Blog</Button>
+        <Button variant="ghost" className="hidden md:block">Write</Button>
+        <Button variant="ghost">Terms</Button>
+        <Button variant="ghost">Polices</Button>
+        <Button variant="ghost">Signin</Button>
+        <Button variant="ghost">Signup</Button>
+      </footer>
     </>
   )
 }
